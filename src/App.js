8397bb3eef32a6a91f0d284251fbef1441ed5b59
Amirh86-dev <birdwolf913@gmail.com>
2025-08-17 02:57:52 +0330

    the project was completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import './App.css';
+import Thermometer from './components/thermometer/Thermometer'
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <Thermometer />
+      </div>
     )
   }
 }
--- src/components/thermometer/Thermometer.jsx
+++ src/components/thermometer/Thermometer.jsx
@@ -1,9 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Thermometer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      temp: 9,
+    };
+  }
+
+  Increase() {
+    this.setState((per) => {
+      return {
+        temp: per.temp + 1,
+      };
+    });
+  }
+
+  Decrease() {
+    this.setState((per) => {
+      return {
+        temp: per.temp - 1,
+      };
+    });
+  }
+
+  ThermometerColor() {
+    if (this.state.temp >= 15) {
+        return "thermometer-text__bg-red"
+    }
+    else if (this.state.temp >= 5) {
+        return "thermometer-text__bg-green"
+    }
+    else if (this.state.temp <= 4) {
+        return "thermometer-text__bg-blue"
+    }
+  }
+
   render() {
+    this.ThermometerColor()
     return (
-      <div>Thermometer</div>
-    )
+      <div className="container">
+        <div className="thermometer-container">
+          <div className={"thermometer-text" + " " + this.ThermometerColor()}>{this.state.temp}</div>
+          <div className="thermometer-btns">
+            <div className="thermometer-btn__increase">
+              <button
+                onClick={(e) => this.Increase(e)}
+                className="btn"
+                type="button"
+              >
+                +
+              </button>
+            </div>
+            <div className="thermometer-btn__decrease">
+              <button
+                onClick={(e) => this.Decrease(e)}
+                className="btn"
+                type="button"
+              >
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
